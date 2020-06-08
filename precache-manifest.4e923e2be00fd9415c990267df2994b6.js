f05344dcb4884b3db0e2c3dcfcd413cc78a321e1
Zhydovlenko <you@example.com>
2020-06-08 16:13:05 +0300

    Updates

---
+++ precache-manifest.4e923e2be00fd9415c990267df2994b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b806f6238bdd4b6b70d111a8932d04d",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "d4d32b8e74abb794cfb8",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.7b1dffb1.chunk.css"
+  },
+  {
+    "revision": "71b0b365a764b30d1f4b",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.61b3de86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.61b3de86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d32b8e74abb794cfb8",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.efdaa215.chunk.js"
+  },
+  {
+    "revision": "de5dfbc78302bed1e7de",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.3cc9cabd.js"
+  }
+]);
